2f8eedca99f6f42522e26d6f63c36d6837e96fc2
Aisya <aisyanwarputri@gmail.com>
2021-03-20 21:21:28 +0700

    list

--- pbo_dart/bin/List&List_Mapping.dart
+++ pbo_dart/bin/List&List_Mapping.dart
@@ -3,6 +3,12 @@ import 'dart:io';
 void main(List<String> arguments) {
     List<int> myList = [];
     List<int> list = [1,2,3];
-    int e = list[1];
-    print(e);
+    // int e = list[1];
+    // print(e);
+
+    for (int index = 0; index < list.length; index += 1) {
+        print(list[index]);
+    }
+
+
 }
